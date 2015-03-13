ab5efe9d108ae8383d24bf8f36928b30fd4e7a58
Borzov <tihoutrom@gmail.com>
2015-03-13 16:02:47 +0500

    Add ALL THE COUNTRIES

---
+++ scripts/make.py
@@ -0,0 +1,6 @@
+cntries = open("flags.txt","rb").readlines()
+template= open("template","rb").read()
+for c in cntries:
+	name, domain = c.rstrip().split(",")
+	out = open(domain+".md","wb")
+	out.write(template.format(name))
